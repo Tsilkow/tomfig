73de850878f95675e606880e38f497837a25db9f
Tomasz Siłkowski <tmsilkowski@gmail.com>
2024-08-14 01:05:01 +0200

    small change

--- configs.py
+++ configs.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import os
 from typing import List
 
 
@@ -11,35 +12,37 @@ class Config(object):
     def __repr__(self):
         return f'{self.name}, {self.filepaths} -> {self.target_directory}'
 
+HOME = os.getenv("HOME")
+
 
 vim_config = Config(
     name='vim', 
     filepaths=['.vimrc'],
-    target_directory='~/'
+    target_directory=HOME,
 )
 
 tmux_config = Config(
     name='tmux', 
     filepaths=['.tmux.conf'],
-    target_directory='~/'
+    target_directory=HOME,
 )
 
 neovim_config = Config(
     name='neovim', 
     filepaths=['init.lua', 'lua/core/', 'lua/plugins/'],
-    target_directory='~/.config/nvim/'
+    target_directory=f'{HOME}/.config/nvim/'
 )
 
 test1_config = Config(
     name='test1',
     filepaths=['file1'],
-    target_directory='~/tomfig/tests/test1/'
+    target_directory=f'{HOME}/tomfig/tests/test1/'
 )
 
 test2_config = Config(
     name='test2',
     filepaths=['file2', 'dir1/'],
-    target_directory='~/tomfig/tests/test2/'
+    target_directory=f'{"HOME"}/tomfig/tests/test2/'
 )
 
 config_sets = {
