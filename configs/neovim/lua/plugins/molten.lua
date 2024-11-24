9136202239e6266321620b171c9f037a5dc42a77
Tomasz Siłkowski <tmsilkowski@gmail.com>
2024-11-24 23:39:20 +0100

    Working molten in nvim

--- configs/neovim/lua/plugins/molten.lua
+++ configs/neovim/lua/plugins/molten.lua
@@ -71,26 +71,24 @@ return {
 },
 {
 	'3rd/image.nvim',
-    config = function()
-    	require('image').setup({
-            backend = "kitty",
-            integrations = {
-                markdown = {
-                    enabled = true,
-                    clear_in_insert_mode = false,
-                    download_remote_images = true,
-                    only_render_image_at_cursor = false,
-                    filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-                }
-            },
-            max_width = 100,
-            max_height = 12,
-            max_height_window_percentage = math.huge,
-            max_width_window_percentage = math.huge,
-            window_overlap_clear_enabled = true,
-            window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-        })
-    end,
+    opts = {
+        backend = "kitty",
+        integrations = {
+            markdown = {
+                enabled = true,
+                clear_in_insert_mode = false,
+                download_remote_images = true,
+                only_render_image_at_cursor = false,
+                filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+            }
+        },
+        max_width = 100,
+        max_height = 12,
+        max_height_window_percentage = math.huge,
+        max_width_window_percentage = math.huge,
+        window_overlap_clear_enabled = true,
+        window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+    },
     dependencies = {
       'leafo/magick',
     },
@@ -103,7 +101,27 @@ return {
     init = function()
         vim.g.molten_image_provider = "image.nvim"
         vim.g.molten_output_win_max_height = 20
+        vim.g.molten_wrap_output = true
+        vim.g.molten_virt_text_output = true
         vim.g.molten_virt_lines_off_by_1 = true
+
+        vim.keymap.set("n", "yy", ":MoltenEvaluateOperator<CR>",
+                { desc = "evaluate operator", silent = true })
+        vim.keymap.set("n", "yo", ":noautocmd MoltenEnterOutput<CR>",
+                { desc = "open output window", silent = true })
+        vim.keymap.set("n", "yr", ":MoltenReevaluateCell<CR>",
+                { desc = "re-eval cell", silent = true })
+        vim.keymap.set("v", "y", ":<C-u>MoltenEvaluateVisual<CR>gv",
+                { desc = "execute visual selection", silent = true })
+        vim.keymap.set("n", "yh", ":MoltenHideOutput<CR>",
+                { desc = "close output window", silent = true })
+        vim.keymap.set("n", "yd", ":MoltenDelete<CR>",
+                { desc = "delete Molten cell", silent = true })
+
+        vim.keymap.set("n", "Y", ":MoltenEvaluateOperator<CR>:call feedkeys(\"ib\")<CR>",
+            { desc = "evaluate cell", silent = true })
+
+
     end,
     -- config = function()
     --     require('molten-nvim').setup({})
--- configs/neovim/lua/plugins/treesitter.lua
+++ configs/neovim/lua/plugins/treesitter.lua
@@ -1,45 +1,85 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    version = false, -- last release is way too old and doesn't work on windows
-    build = ":tsupdate",
-    config = function ()
-        local configs = require("nvim-treesitter.configs")
+    {
+        "nvim-treesitter/nvim-treesitter",
+        version = false, -- last release is way too old and doesn't work on windows
+        build = ":tsupdate",
+        config = function ()
+            local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-            ensure_installed = {
-                "bash",
-                "cpp",
-                "diff",
-                "dockerfile",
-                "html",
-                "javascript",
-                "json",
-                "lua",
-                "luadoc",
-                "luap",
-                "markdown",
-                "markdown_inline",
-                "printf",
-                "python",
-                "query",
-                "regex",
-                "toml",
-                "vim",
-                "vimdoc",
-                "xml",
-                "yaml",
-            },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    node_incremental = "s",
-                    node_decremental = "S",
+            configs.setup({
+                ensure_installed = {
+                    "bash",
+                    "cpp",
+                    "diff",
+                    "dockerfile",
+                    "html",
+                    "javascript",
+                    "json",
+                    "lua",
+                    "luadoc",
+                    "luap",
+                    "markdown",
+                    "markdown_inline",
+                    "printf",
+                    "python",
+                    "query",
+                    "regex",
+                    "toml",
+                    "vim",
+                    "vimdoc",
+                    "xml",
+                    "yaml",
                 },
-            },
-        })
-    end
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        node_incremental = "s",
+                        node_decremental = "S",
+                    },
+                },
+                textobjects = {
+                    move = {
+                        enable = true,
+                        set_jumps = false, -- you can change this if you want.
+                        goto_next_start = {
+                            --- ... other keymaps
+                            ["]b"] = { query = "@code_cell.inner", desc = "next code block" },
+                        },
+                        goto_previous_start = {
+                            --- ... other keymaps
+                            ["[b"] = { query = "@code_cell.inner", desc = "previous code block" },
+                        },
+                    },
+                    select = {
+                        enable = true,
+                        lookahead = true, -- you can change this if you want
+                        keymaps = {
+                            --- ... other keymaps
+                            ["ib"] = { query = "@code_cell.inner", desc = "in block" },
+                            ["ab"] = { query = "@code_cell.outer", desc = "around block" },
+                        },
+                    },
+                    swap = { -- Swap only works with code blocks that are under the same
+                            -- markdown header
+                        enable = true,
+                        swap_next = {
+                            --- ... other keymap
+                            ["<leader>sbl"] = "@code_cell.outer",
+                        },
+                        swap_previous = {
+                            --- ... other keymap
+                            ["<leader>sbh"] = "@code_cell.outer",
+                        },
+                    },
+                }
+            })
+        end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    }
 }
 
