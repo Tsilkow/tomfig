f412e587abd9e846f4aa22b20c4e316deaad1eae
Tsilkow <tmsilkowski@gmail.com>
2024-12-12 15:30:40 +0100

    changed kitty font, minor changes in nvim and vim

--- configs/neovim/lua/plugins/telescope.lua
+++ configs/neovim/lua/plugins/telescope.lua
@@ -1,22 +1,38 @@
 return {
-{
+  {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', 'e', builtin.find_files,
-            { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<leader>e', builtin.live_grep,
-            { desc = 'Telescope live grep' })
-        vim.keymap.set('n', 'b', builtin.buffers,
-            { desc = 'Telescope buffers' })
-        vim.keymap.set('n', '<leader>b', builtin.help_tags,
-            { desc = 'Telescope help tags' })
+      local telescope = require('telescope')
+      local builtin = require('telescope.builtin')
+      local actions = require("telescope.actions")
+
+      vim.keymap.set('n', 'e', builtin.find_files,
+        { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<leader>e', builtin.live_grep,
+        { desc = 'Telescope live grep' })
+      vim.keymap.set('n', 'b', builtin.buffers,
+        { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>b', builtin.help_tags,
+        { desc = 'Telescope help tags' })
+
+      telescope.defaults = telescope.defaults or {}
+      telescope.defaults.mappings = telescope.defaults.mappings or {}
+
+      telescope.defaults.mappings.i = vim.tbl_extend("force", telescope.defaults.mappings.i or {}, {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+      })
+
+      telescope.defaults.mappings.n = vim.tbl_extend("force", telescope.defaults.mappings.n or {}, {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+      })
     end
-},
-{
+  },
+  {
     'stevearc/dressing.nvim',
     opts = {},
-},
+  },
 }
