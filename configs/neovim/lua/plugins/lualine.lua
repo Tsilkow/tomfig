e2ee7150c0acd971a0fd2c99e54af6d39d2fb95b
tsilkowski <tmsilkowski@gmail.com>
2025-04-13 12:29:07 +0200

    Changed kitty theme and tweaked lsp and lualine

--- configs/neovim/lua/plugins/lualine.lua
+++ configs/neovim/lua/plugins/lualine.lua
@@ -1,6 +1,23 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    config = function()
-    	require('lualine').setup({})
-    end
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+		})
+	end,
 }
